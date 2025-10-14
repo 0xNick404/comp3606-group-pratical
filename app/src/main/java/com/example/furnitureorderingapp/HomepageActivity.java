3fbcb1cbe83340e826ea268f670d04a9591fe147
0xNick404 <ngrimestt@gmail.com>
2025-10-14 13:50:57 -0400

    Create HomepageActivity.java

---
+++ app/src/main/java/com/example/furnitureorderingapp/HomepageActivity.java
@@ -0,0 +1,29 @@
+package com.example.furnitureorderingapp;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class HomepageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_homepage);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        String name = getIntent().getStringExtra("USER_FULL_NAME");
+
+        ((TextView)findViewById(R.id.tvGreeting)).setText("Welcome back " + name + "!");
+    }
+}
